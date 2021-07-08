ecabf26637c2d576baca8f314063f6d63f3e6e08
joseph.lenton@askattest.com <joseph.lenton@askattest.com>
2021-07-08 22:47:46 +0100

    feat: add NumberExtensions::scale_percentage

--- src/num/number_extensions.rs
+++ src/num/number_extensions.rs
@@ -1,3 +1,5 @@
+use ::std::ops::Range;
+
 pub trait NumberExtensions {
     fn range_transform(self, src_from: Self, src_to: Self, dest_from: Self, dest_to: Self) -> Self;
 
@@ -7,6 +9,16 @@ pub trait NumberExtensions {
      * 1 - self.
      */
     fn inverse(self) -> Self;
+
+    /**
+     * For use on 0.0..=1.0 ranges, to transform into a different
+     * 0.0..=1.0 range.
+     *
+     *  - If the number is below the range, then 0.0 is returned.
+     *  - If the number is above the range, then 1.0 is returned.
+     *  - Between these n will return 0.0 to 1.0.
+     */
+    fn scale_percentage(self, range: Range<f32>) -> f32;
 
     /**
      * Returns self limited to between min and max.
@@ -20,8 +32,20 @@ impl NumberExtensions for f32 {
         percent.percentage_transform(dest_from, dest_to)
     }
 
+    /// From converting from 0.0 to 1.0, to say 20.0 to 50.0.
     fn percentage_transform(self, from: Self, to: Self) -> Self {
         from + (self * (to - from))
+    }
+
+    fn scale_percentage(self, range: Range<Self>) -> Self {
+        if self < range.start {
+            0.0
+        } else if range.end < self {
+            1.0
+        } else {
+            let diff = range.end - range.start;
+            (self - range.start) / diff
+        }
     }
 
     fn inverse(self) -> Self {

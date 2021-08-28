b9f8cea50f2b936c4ed89ab5838b66fdee85882a
joseph.lenton@askattest.com <joseph.lenton@askattest.com>
2021-08-28 18:34:04 +0100

    refactor: remove unused import

--- src/num/num_tuple.rs
+++ src/num/num_tuple.rs
@@ -9,7 +9,6 @@ use std::ops::Sub;
 use std::ops::SubAssign;
 
 use crate::num::Num;
-use crate::num::ToRounded;
 
 /// This exists to help add some common functionality to things that
 /// are a tuple of two numeric items.

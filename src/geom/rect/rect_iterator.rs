335b294216a5ea4dbbb9e312622c7f46f80d7568
josephlenton@gmail.com <josephlenton@gmail.com>
2022-02-09 20:19:39 +0000

    fix: Rect no longer iterates when zero width or height

--- src/geom/rect/rect_iterator.rs
+++ src/geom/rect/rect_iterator.rs
@@ -23,6 +23,14 @@ impl<N: Num> Iterator for RectIterator<N> {
     type Item = Point<N>;
 
     fn next(&mut self) -> Option<Self::Item> {
+        if self.bottom_left.x() == self.top_right.x() {
+            return None;
+        }
+
+        if self.bottom_left.y() == self.top_right.y() {
+            return None;
+        }
+
         if self.top_right.x() <= self.current.x() {
             self.current = Point(self.bottom_left.x(), self.current.y() + N::one());
         }
@@ -41,23 +49,70 @@ impl<N: Num> Iterator for RectIterator<N> {
 mod iterating {
     use super::*;
     use crate::geom::Size;
+    use ::testcat::*;
 
-    #[test]
-    fn it_should_not_iterate_empty_rect() {
-        let mut xs = vec![];
-        let mut ys = vec![];
+    it!("should not iterate over empty rect", test_empty_rect);
+    it!(
+        "should not iterate over rect with zero width",
+        test_rect_with_no_width
+    );
+    it!(
+        "should not iterate over rect with zero height",
+        test_rect_with_no_height
+    );
+    it!(
+        "should return one value for a rect with 1x1 size",
+        test_rect_with_size_one
+    );
+    it!(
+        "should return one value for a rect with negative 1x1 size",
+        test_rect_with_negative_size_one
+    );
+    it!(
+        "should iterate over rect with width and height",
+        test_rect_with_area
+    );
+    it!(
+        "should iterate over rect with width and height, for usize",
+        test_rect_with_area_usize
+    );
 
-        for Point(x, y) in Rect(Point(2, 3), Size(0, 0)) {
-            xs.push(x);
-            ys.push(y);
-        }
+    fn test_empty_rect() {
+        let rect: Rect<usize> = Rect(Point(2, 3), Size(0, 0));
+        let points: Vec<Point<usize>> = rect.into_iter().collect();
 
-        assert_eq!(xs, []);
-        assert_eq!(ys, []);
+        assert_eq!(points, []);
     }
 
-    #[test]
-    fn it_should_iterate_over_rect() {
+    fn test_rect_with_no_width() {
+        let rect: Rect<usize> = Rect(Point(2, 3), Size(0, 4));
+        let points: Vec<Point<usize>> = rect.into_iter().collect();
+
+        assert_eq!(points, []);
+    }
+
+    fn test_rect_with_no_height() {
+        let rect: Rect<usize> = Rect(Point(2, 3), Size(3, 0));
+        let points: Vec<Point<usize>> = rect.into_iter().collect();
+
+        assert_eq!(points, []);
+    }
+
+    fn test_rect_with_size_one() {
+        let rect: Rect<usize> = Rect(Point(2, 3), Size(1, 1));
+        let points: Vec<Point<usize>> = rect.into_iter().collect();
+
+        assert_eq!(points, [Point(2, 3)]);
+    }
+
+    fn test_rect_with_negative_size_one() {
+        let rect: Rect<isize> = Rect(Point(2, 3), Size(-1, -1));
+        let points: Vec<Point<isize>> = rect.into_iter().collect();
+
+        assert_eq!(points, [Point(1, 2)]);
+    }
+
+    fn test_rect_with_area() {
         let mut xs = vec![];
         let mut ys = vec![];
 
@@ -83,8 +138,7 @@ mod iterating {
         ]);
     }
 
-    #[test]
-    fn it_should_iterate_over_rect_usize() {
+    fn test_rect_with_area_usize() {
         let mut xs = vec![];
         let mut ys = vec![];
 
